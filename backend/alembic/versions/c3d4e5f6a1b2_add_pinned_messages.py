1602e71d61649fad9f0dfdbc66c66e453a2eff0c
pheineke <heineke.pe@gmail.com>
2026-02-22 20:45:19 +0100

    fix: correct down_revision in add_pinned_messages migration

--- backend/alembic/versions/c3d4e5f6a1b2_add_pinned_messages.py
+++ backend/alembic/versions/c3d4e5f6a1b2_add_pinned_messages.py
@@ -11,7 +11,7 @@ from alembic import op
 import sqlalchemy as sa
 
 revision: str = 'c3d4e5f6a1b2'
-down_revision: Union[str, None] = 'b2c3d4e5f6a1'
+down_revision: Union[str, None] = 'd4e5f6a7b8c9'
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
 
